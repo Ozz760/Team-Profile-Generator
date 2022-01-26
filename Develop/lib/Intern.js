6f634d26c9cd0a4e611e58132a89c33a953e4b4c
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-25 17:52:19 -0800

    update js

---
+++ Develop/lib/Intern.js
@@ -0,0 +1,17 @@
+const Employee = require("./Employee")
+
+class Intern extends Employee { 
+    constructor (school) { 
+        super (name, id, email); 
+        this.school = school; 
+    }
+
+    getSchool() { 
+
+    }
+
+    getRole() { 
+        
+    }
+
+}
