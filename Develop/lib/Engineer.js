3839345ec51c0bbe832e8922ef17c2f686116180
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-25 22:15:09 -0800

    js Update

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,17 +1,20 @@
 const Employee = require("./Employee")
 
 class Engineer extends Employee { 
-    constructor (github) {
+    constructor (name, id, email, github) {
         super (name, id, email); 
-        this. github = github; 
+        this.github = github; 
     }
 
     getGithub() { 
+        return this.github;
 
     }
 
     getRole() { 
+        return "Engineer"; 
         
     }
 
 }
+module.exports = Engineer; 
