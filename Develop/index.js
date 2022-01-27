f1c8dcb478c5dcba5a9efbb52cd89746bf4d2a38
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-26 21:25:54 -0800

    Prettier

--- Develop/index.js
+++ Develop/index.js
@@ -1,131 +1,123 @@
-const inquirer = require('inquirer'); 
-const fs = require('fs'); 
-const Employee = require ('./lib/Employee'); 
-const Engineer = require('./lib/Engineer'); 
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager'); 
-const Choices = require('inquirer/lib/objects/choices');
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Choices = require("inquirer/lib/objects/choices");
 
 class Prompt {
-    // Manager prompt 
-    beginManager() {
-        inquirer
-        .prompt ([
-            {
-                type: "input",
-                name: "managerName", 
-                message: "Who is the Team Manager name?", 
-            },
-            {
-                type: "input", 
-                name: "mangerId",
-                message: "What is the Team Manager Id?"
-            },
-            {
-                type: "input", 
-                name: "mangerOfficeNo",
-                message: "What is the Team Manager Office Number?"
-            }, 
-            {
-                type: "input", 
-                name: "mangerEmail",
-                message: "What is the Team Manager Email?"
-            }
-        ])
-        .then(({addAnotherEmployee}) => {
-            this.addEmployee(addAnotherEmployee); 
-        });
-    };
-    // Employee prompt
+  // Manager prompt
+  beginManager() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "Who is the Team Manager name?",
+        },
+        {
+          type: "input",
+          name: "mangerId",
+          message: "What is the Team Manager Id?",
+        },
+        {
+          type: "input",
+          name: "mangerOfficeNo",
+          message: "What is the Team Manager Office Number?",
+        },
+        {
+          type: "input",
+          name: "mangerEmail",
+          message: "What is the Team Manager Email?",
+        },
+      ])
+      .then(({ addAnotherEmployee }) => {
+        this.addEmployee(addAnotherEmployee);
+      });
+  }
+  // Employee prompt
 
-    addEmployee() {
-        inquirer
-        .prompt ([
-            {
-                type: "list",
-                name: "employeeType", 
-                message: "Choose the type of Employee",
-                choices: [
-                    {name: "Engineer"},
-                    {name: "Intern" },
-                ],
-            }, 
-        ])
-        .then(({getEngineer, getIntern}) => {
-            if (getEngineer) {
-                this.addEngineer
-            } else if (getIntern) {
-                this.addIntern
-            } else {
-                this.writeHtml
-            }
-        });
-    }; 
+  addEmployee() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "employeeType",
+          message: "Choose the type of Employee",
+          choices: [{ name: "Engineer" }, { name: "Intern" }],
+        },
+      ])
+      .then(({ getEngineer, getIntern }) => {
+        if (getEngineer) {
+          this.addEngineer;
+        } else if (getIntern) {
+          this.addIntern;
+        } else {
+          this.writeHtml;
+        }
+      });
+  }
 
-    addEngineer() {
-        inquirer
-        .prompt ([
-            {
-                type: "input",
-                name: "engineerName", 
-                message: "What is the Engineer's name?", 
-            },
-            {
-                type: "input", 
-                name: "engineerId",
-                message: "What is the Engineer Id?"
-            },
-            {
-                type: "input", 
-                name: "engineerEmail",
-                message: "What is the Engineer Email?"
-                
-            }, 
-            {
-                type: "input", 
-                name: "engineerGitHub",
-                message: "What is the Engineer GitHub?"
-            }
-        ])
-        .then (({backToEmployee}) => {
-            this.addEmployee(backToEmployee); 
-        });
-    };
+  addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is the Engineer's name?",
+        },
+        {
+          type: "input",
+          name: "engineerId",
+          message: "What is the Engineer Id?",
+        },
+        {
+          type: "input",
+          name: "engineerEmail",
+          message: "What is the Engineer Email?",
+        },
+        {
+          type: "input",
+          name: "engineerGitHub",
+          message: "What is the Engineer GitHub?",
+        },
+      ])
+      .then(({ backToEmployee }) => {
+        this.addEmployee(backToEmployee);
+      });
+  }
 
-    addIntern() { 
-        inquirer
-        .prompt ([
-            {
-                type: "input",
-                name: "internName", 
-                message: "What is the Intern's name?", 
-            },
-            {
-                type: "input", 
-                name: "internId",
-                message: "What is the Intern Id?"
-            },
-            {
-                type: "input", 
-                name: "internEmail",
-                message: "What is the Intern Email?"
-                
-            }, 
-            {
-                type: "input", 
-                name: "internSchool",
-                message: "Which school does the Intern go to?"
-            }
-        ])
-        .then (({backToEmployee}) => {
-            this.addEmployee(backToEmployee); 
-        });
-    }; 
-     // Write HTML 
-    writeHtml() {
-        
-    }
-    }; 
+  addIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "What is the Intern's name?",
+        },
+        {
+          type: "input",
+          name: "internId",
+          message: "What is the Intern Id?",
+        },
+        {
+          type: "input",
+          name: "internEmail",
+          message: "What is the Intern Email?",
+        },
+        {
+          type: "input",
+          name: "internSchool",
+          message: "Which school does the Intern go to?",
+        },
+      ])
+      .then(({ backToEmployee }) => {
+        this.addEmployee(backToEmployee);
+      });
+  }
+  // Write HTML
+  writeHtml() {}
+}
 
-module.exports = Prompt;  
+module.exports = Prompt;
