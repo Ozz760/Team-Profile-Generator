9b3da09ef3522c2d99396187e8d3296d404a9b25
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-27 23:50:50 -0800

    little update to tthe engineer promtps and index.js

---
+++ Develop/index.js
@@ -0,0 +1,3 @@
+const Prompt = require('./dist/template');
+
+new Prompt().buildRoster();
--- Develop/src/EngineerTemp.js
+++ Develop/src/EngineerTemp.js
@@ -3,10 +3,9 @@ const EngineerTemp = (engineer) =>
       return`<div class="card-body">
       <h5 class="card-title">${item.getName()}</h5>
       <h5>${item.getRole()}</h5>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
-  <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z"/>
-  <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z"/>
-  </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyeglasses" viewBox="0 0 16 16">
+      <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+      </svg> 
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">ID:${item.getId()}</li>
